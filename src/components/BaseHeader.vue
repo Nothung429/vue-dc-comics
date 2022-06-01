33aa8c77f05d3ab96c89b215278d560891e6775e
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-01 15:54:08 +0200

    add html &  js

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -14,67 +14,67 @@
 </template>
 
 <script>
-export default {
-    name: 'BaseHeader',
-    data() {
-        return {
-            links: [
-                {
-                    text: "characters",
-                    url: "#",
-                    current: false,
-                },
-                {
-                    text: "comics",
-                    url: "#",
-                    current: true,
-                },
-                {
-                    text: "movies",
-                    url: "#",
-                    current: false,
-                },
-                {
-                    text: "tv",
-                    url: "#",
-                    current: false,
-                },
-                {
-                    text: "games",
-                    url: "#",
-                    current: false,
-                },
-                {
-                    text: "collectibles",
-                    url: "#",
-                    current: false,
-                },
-                {
-                    text: "videos",
-                    url: "#",
-                    current: false,
-                },
-                {
-                    text: "fans",
-                    url: "#",
-                    current: false,
-                },
-                {
-                    text: "news",
-                    url: "#",
-                    current: false,
-                },
-                {
-                    text: "shop",
-                    url: "#",
-                    current: false,
-                },
-            ],
-        }
-    },
-}
+    export default {
+        name: 'BaseHeader',
+        data() {
+            return {
+                links: [
+                    {
+                        text: "characters",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "comics",
+                        url: "#",
+                        current: true,
+                    },
+                    {
+                        text: "movies",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "tv",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "games",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "collectibles",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "videos",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "fans",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "news",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "shop",
+                        url: "#",
+                        current: false,
+                    },
+                ],
+            }
+        },
+    }
 </script>
 
 <style>
-    
+
 </style>
