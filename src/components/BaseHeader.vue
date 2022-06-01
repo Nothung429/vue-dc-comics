ede3980bf383e3cfe654ad591cc81de012237962
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-01 15:23:17 +0200

    add vue files

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,80 @@
+<template>
+    <header>
+        <div class="header__logo">
+            <img src="../assets/img/dc-logo.png">
+        </div>
+        <div class="header__nav">
+            <ul>
+                <li v-for="(link, index) in links" :key="index" :class="{active : link.current}">
+                    <a :href="link.url">{{link.text}}</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'BaseHeader',
+    data() {
+        return {
+            links: [
+                {
+                    text: "characters",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "comics",
+                    url: "#",
+                    current: true,
+                },
+                {
+                    text: "movies",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "tv",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "games",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "collectibles",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "videos",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "fans",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "news",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "shop",
+                    url: "#",
+                    current: false,
+                },
+            ],
+        }
+    },
+}
+</script>
+
+<style>
+    
+</style>
---
+++ src/components/BaseMain.vue
@@ -0,0 +1,65 @@
+<template>
+    <main>
+        <div class="wip">
+            <h3>--> Content go here</h3>
+        </div>
+        <div class="main__nav">
+            <ul>
+                <li v-for="(link, index) in links" :key="index" :class="{active : link.current}">
+                    <img :src="link.img" alt="main-li-img">
+                    <a :href="link.url">{{link.text}}</a>
+                </li>
+            </ul>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'BaseMain',
+    data() {
+        return {
+            links: [
+                {
+                    text: "digital comics",
+                    url: "#",
+                    img: "buy-comics-digital-comics.png",
+                    current: false,
+                },
+                {
+                    text: "dc merchandise",
+                    url: "#",
+                    img: "../assets/img/buy-comics-merchandise.png",
+                    current: true,
+                },
+                {
+                    text: "subscription",
+                    url: "#",
+                    img: "../assets/img/buy-comics-subscriptions.png",
+                    current: false,
+                },
+                {
+                    text: "comic shop location",
+                    url: "#",
+                    img: "../assets/img/buy-comics-shop-locator.png",
+                    current: false,
+                },
+                {
+                    text: "dc power visa",
+                    url: "#",
+                    img: "../assets/img/buy-dc-power-visa.png",
+                    current: false,
+                },
+            ],
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+    .wip {
+        background-color: red;
+        width: 100%;
+        height: 100px;
+    }
+</style>
