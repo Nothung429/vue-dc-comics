49b96b106c179e02222cc1e1803068c68c4989d1
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-01 14:34:47 +0200

    add files vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        
+    </div>
+</template>
+
+<script>
+import BaseHeader from './components/BaseHeader.vue'
+import BaseMain from './components/BaseMain.vue'
+import BaseFooter from './components/BaseFooter.vue'
+
+export default {
+    name: 'App',
+    components: {
+        BaseHeader,
+        BaseMain,
+        BaseFooter
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
